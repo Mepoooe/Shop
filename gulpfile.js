4e58ee7246b0afce6c7b518e57ea9e3b43e03cd1
Misha Nikula <cj27111992@gmail.com>
2016-02-13 21:22:55 +0200

    work 2 started

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require("gulp"),
  browserSync = require('browser-sync'),
- modernizr = require('gulp-modernizr');
+ modernizr = require('gulp-modernizr'),
+ jade = require('gulp-jade'),
+ plumber = require('gulp-plumber');
+
+ var jadePath = './app/jade/*jade';
 
 gulp.task('modernizr', function(){
 	gulp.src('app/js/*.js')
@@ -23,6 +27,18 @@ gulp.task('modernizr', function(){
 	.pipe(gulp.dest("app/js/vendor"))
 });
 
+gulp.task('jade', function(){
+	var YOUR_LOCALS = {};//можно  подключить JSON с данными
+
+	gulp.src(jadePath)
+		.pipe(plumber())
+		.pipe(jade({
+			locls: YOUR_LOCALS,
+			pretty: '\t'
+		}))
+		.pipe(gulp.dest('./app/html/'))
+});
+
 gulp.task('server', function(){
  browserSync({
   port: 9000,
@@ -36,7 +52,8 @@ gulp.task('watch', function(){
  gulp.watch([
   'app/*.html',
   'app/js/**/*.js',
-  'app/css/**/*.css'
+  'app/css/**/*.css',
+  jadePath, ['jade']
   ]).on('change', browserSync.reload);
 });
 
